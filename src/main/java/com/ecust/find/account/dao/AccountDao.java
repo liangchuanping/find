ab495a27fdad0a5ab75eeb91c2fdfc36774f5ca2
Liangchuan.Ping <Liangchuan.Ping@172.21.206.253>
2017-03-09 20:41:24 +0800

    add the framework of bootstrap and jquery
    Change-Id: Ifef66387d4e819be98410efad5832d96c94b0cad
    Signed-off-by: Liangchuan.Ping <Liangchuan.Ping@172.21.206.253>

---
+++ src/main/java/com/ecust/find/account/dao/AccountDao.java
@@ -0,0 +1,9 @@
+package com.ecust.find.account.dao;
+
+public interface AccountDao{
+	/*insert a visit record 
+	 * @studentId the ID that visit.
+	 */
+	public void recordVisit(String studentId);
+	
+}
